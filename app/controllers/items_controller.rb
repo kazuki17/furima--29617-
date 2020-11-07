794e46fc5a91d2112108a017ada10ec82db3f3cf
kazuki17 <71236928+kazuki17@users.noreply.github.com>
2020-11-07 15:23:30 +0900

    関連しない実装部分のコメントアウト
    それぞれの実装に関連しない部分の記述をコメントアウトしました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,38 +1,38 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update]
+  # before_action :set_item, only: [:edit, :update]
  
   def index
-  @items = Item.all
-  @items = Item.order("created_at DESC")
+  # @items = Item.all
+  # @items = Item.order("created_at DESC")
   end
  
- def new
+#  def new
   
-  @items = Item.new
- end
+#   @items = Item.new
+#  end
 
- def show
- end
+#  def show
+#  end
 
- def edit
- end
+#  def edit
+#  end
 
- def update
-  @items.update(message_params)
-  redirect_to root_path
- end
+#  def update
+#   @items.update(message_params)
+#   redirect_to root_path
+#  end
 
- def create
-  Item.create(item_params)
- end
+#  def create
+#   Item.create(item_params)
+#  end
 
- private
- def item_params
-  params.require(:item).permit(:image, :text).
- end
+#  private
+#  def item_params
+#   params.require(:item).permit(:image, :text).
+#  end
 
- def set_item
-  @items = Item.find(params[:id])
- end
-end 
+#  def set_item
+#   @items = Item.find(params[:id])
+#  end
+# end 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,15 @@
 class Item < ApplicationRecord
-  has_one_attached :image
-  validates :text, presence: true
-  validates :image, presence: true
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :item_category
+  # has_one_attached :image
+  # validates :text, presence: true
+  # validates :image, presence: true
+  # extend ActiveHash::Associations::ActiveRecordExtensions
+  # belongs_to_active_hash :item_category
 
   #空の投稿を保存できないようにする
-  validates :title, :name, :item_category, presence: true
+  # validates :title, :name, :item_category, presence: true
 
   #選択が「--」のままになっていないか
-  with_options numericality: { other_than: 1 } do
-    validates :item_category_id
-  end
+  # with_options numericality: { other_than: 1 } do
+  #   validates :item_category_id
+  # end
 end
