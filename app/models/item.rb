422adcd740239b10d43b7088b5ea88eb6db2ec84
kazuki17 <71236928+kazuki17@users.noreply.github.com>
2020-11-08 16:41:41 +0900

    rubocopの修正？

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,10 +5,10 @@ class Item < ApplicationRecord
   # extend ActiveHash::Associations::ActiveRecordExtensions
   # belongs_to_active_hash :item_category
 
-  #空の投稿を保存できないようにする
+  # 空の投稿を保存できないようにする
   # validates :title, :name, :item_category, presence: true
 
-  #選択が「--」のままになっていないか
+  # 選択が「--」のままになっていないか
   # with_options numericality: { other_than: 1 } do
   #   validates :item_category_id
   # end
