625fe783448e3aebbd634a51986322d6310b74fb
kazuki17 <71236928+kazuki17@users.noreply.github.com>
2020-11-08 16:38:27 +0900

    ユーザー登録　バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_date, presence: true
+  validates :remember_created_at, presence: true
+
+  # アソシエーションの定義
+  has_many   :items
+  has_many   :purchase_historys
 end
