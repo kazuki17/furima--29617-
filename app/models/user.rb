a0f4252e606c213a066581db42a10fc74a429e0e
kazuki17 <71236928+kazuki17@users.noreply.github.com>
2020-11-09 19:32:04 +0900

    ユーザー登録　修正③

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,16 @@ class User < ApplicationRecord
  with_options presence: true do
   validates :nickname
   validates :email
-  validates :password  
+  validates :password         format: { with: /\A[a-z0-9]+\z/i, message: "は半角英数で入力してください。"}
   validates :password_confirmation
-  validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  validates :last_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+
+  name = /\A[ぁ-んァ-ン一-龥]/
+  kana = /\A[ァ-ヶー－]+\z/
+
+  validates :last_name,       format: { with: name, message: "is invalid. Input full-width characters."}
+  validates :first_name,      format: { with: name, message: "is invalid. Input full-width characters."} 
+  validates :last_name_kana,  format: { with: kana, message: "is invalid. Input full-width katakana characters."}
+  validates :first_name_kana, format: { with: kana, message: "is invalid. Input full-width katakana characters."}
   validates :birth_date
  end 
 
