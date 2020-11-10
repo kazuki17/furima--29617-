d6cecdd206d588dac7dbc09b08d58cd4d3f6652f
kazuki17 <71236928+kazuki17@users.noreply.github.com>
2020-11-10 16:10:02 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,14 @@
 class ItemsController < ApplicationController
-  # before_action :set_item, only: [:edit, :update]
-  before_action :move_to_index, except: [:index, :show]
+  #before_action :set_item, only: [:edit, :update]
+  #before_action :move_to_index, except: [:index, :show]
 
   def index
     #@items = Item.order('created_at DESC')
   end
 
-  # def new
-  #   @items = Item.new
-  # end
+   def new
+     @item = Item.new
+   end
 
   # def show
   # end
@@ -22,17 +22,24 @@ class ItemsController < ApplicationController
   # end
 
   def create
-    Item.create(item_params)
+    @items = Item.new(item_params)
+   #バリデーションで問題があれば、保存はされず「投稿画面」に戻る
+   if @items.valid?
+     @items.save
+     redirect_to root_path
+   else
+     #保存されなければ、newに戻る
+     render 'new'
+   end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image, :text)
-          .end
+    params.require(:item).permit(:name, :text, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :user_id)
+          end
 
-    # def set_item
-    #   @items = Item.find(params[:id])
-    # end
-  end
+     #def set_item
+      # @items = Item.find(params[:id])
+     #end
 end
