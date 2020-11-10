0d68b71e8fa8369f3b5aabd5ce3ccbe40d344b55
kazuki17 <71236928+kazuki17@users.noreply.github.com>
2020-11-10 16:09:24 +0900

    テーブル設計

--- db/migrate/20201028095612_create_items.rb
+++ db/migrate/20201028095612_create_items.rb
@@ -3,13 +3,13 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.text       :name,                         null: false
       t.text       :text,                         null: false
-      t.inetger    :category_id,                  null: false
-      t.inetger    :sales_status_id,              null: false
-      t.inetger    :shipping_fee_status_id,       null: false
+      t.integer    :category_id,                  null: false
+      t.integer    :sales_status_id,              null: false
+      t.integer    :shipping_fee_status_id,       null: false
       t.integer    :prefecture_id,                null: false
-      t.inetger    :scheduled_delivery_id,        null: false
-      t.inetger    :price,                        null: false
-      t.inetger    :user_id,                      foreign_key: true
+      t.integer    :scheduled_delivery_id,        null: false
+      t.integer    :price,                        null: false
+      t.integer    :user_id,                      foreign_key: true
       t.timestamps   
     end
   end
