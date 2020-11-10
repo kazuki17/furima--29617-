e40b3409ab3c995950276828e0bb64b7384aafcc
kazuki17 <71236928+kazuki17@users.noreply.github.com>
2020-11-10 16:08:01 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,29 @@
 class Item < ApplicationRecord
-  # has_one_attached :image
-  # validates :text, presence: true
-  # validates :image, presence: true
-  # extend ActiveHash::Associations::ActiveRecordExtensions
-  # belongs_to_active_hash :item_category
+   has_one_attached :image
 
-  # 空の投稿を保存できないようにする
-  # validates :title, :name, :item_category, presence: true
+   with_options presence: true do
+    validates :name
+    validates :text
+    validates :category_id         
+    validates :sales_status_id
+    validates :shipping_fee_status_id       
+    validates :prefecture_id      
+    validates :scheduled_delivery_id  
+    validates :price
+    validates :user_id
+   end 
 
-  # 選択が「--」のままになっていないか
-  # with_options numericality: { other_than: 1 } do
-  #   validates :item_category_id
-  # end
+   extend ActiveHash::Associations::ActiveRecordExtensions
+   belongs_to_active_hash :category
+   belongs_to_active_hash :sales_status
+   belongs_to_active_hash :shipping_fee_status
+   belongs_to_active_hash :prefecture
+   belongs_to_active_hash :scheduled_delivery
+   #空の投稿を保存できないようにする
+   
+
+   #選択が「--」のままになっていないか
+   with_options numericality: { other_than: 1 } do
+     validates :category
+   end
 end
