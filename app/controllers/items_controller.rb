a80607527e4f40107c5b3f79c6f1a9077ac81680
kazuki17 <71236928+kazuki17@users.noreply.github.com>
2020-11-15 09:37:09 +0900

    再提出　条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
   before_action :authenticate_user!, only: [:new]
+  before_action :move_to_index, except: [:index]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -45,5 +46,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
